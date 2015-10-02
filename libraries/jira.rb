52f8e5fac58337e4f116e8a681359647c2e6b9af
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-10-02 16:39:22 -0700

    Fixed library error.

--- libraries/jira.rb
+++ libraries/jira.rb
@@ -5,35 +5,49 @@ class Chef
     # Chef::Recipe::Jira class
     class Jira
       def self.settings(node)
-        begin
-          if Chef::Config[:solo]
-            begin
-              settings = Chef::DataBagItem.load('jira', 'jira')['local']
-            rescue
-              Chef::Log.info('No jira data bag found')
-            end
-          else
-            begin
-              settings = Chef::EncryptedDataBagItem.load('jira', 'jira')[node.chef_environment]
-            rescue
-              Chef::Log.info('No jira encrypted data bag found')
-            end
-          end
-        ensure
-          settings ||= node['jira']
+        @@settings_from_data_bag ||= settings_from_data_bag(node)
+        settings = Chef::Mixin::DeepMerge.deep_merge(
+          @@settings_from_data_bag,
+          node[cookbook_name].to_hash
+        )
 
-          case settings['database']['type']
-          when 'mysql'
-            settings['database']['port'] ||= 3306
-          when 'postgresql'
-            settings['database']['port'] ||= 5432
-          else
-            Chef::Log.warn('Unsupported database type.')
-          end
+        case settings['database']['type']
+        when 'mysql'
+          settings['database']['port'] ||= 3306
+        when 'postgresql'
+          settings['database']['port'] ||= 5432
+        else
+          fail "Unsupported database type: #{settings['database']['type']}"
         end
 
         settings
+
+
       end
+
+      # Fetchs application settings from the data bag
+      #
+      # @return [Hash] Settings hash
+      def self.settings_from_data_bag(node)
+        item =
+        begin
+          if Chef::Config[:solo]
+            Chef::DataBagItem.load(cookbook_name, cookbook_name)['local']
+          else
+            Chef::EncryptedDataBagItem.load(cookbook_name, cookbook_name)[node.chef_environment]
+          end
+        rescue
+          Chef::Log.info("No #{cookbook_name} data bag found")
+          nil
+        end
+
+        item || {}
+      end
+
+      def self.cookbook_name
+        'jira'
+      end
+
     end
   end
 end
